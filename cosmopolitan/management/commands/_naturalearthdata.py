b3ae333c5c1d2beaa187c0941a0fbeec75c77f67
Ekaterina Krivich <kiote@Ekaterinas-MacBook-Pro.local>
2016-02-15 12:30:19 +0300

    Add some checks for downloaded file

--- cosmopolitan/management/commands/_naturalearthdata.py
+++ cosmopolitan/management/commands/_naturalearthdata.py
@@ -1,12 +1,18 @@
-import zipfile
-from urllib import request
 import os
+import zipfile
+
+from urllib import request
 
 HOST = "http://naciscdn.org"
 COUNTRIES = "/naturalearth/110m/cultural/"
 COUNTRIES_FILE_NAME_NO_EXT = "ne_110m_admin_0_countries"
 COUNTRIES_FILE_NAME = COUNTRIES_FILE_NAME_NO_EXT + ".zip"
 FOLDER = "data/"
+
+def super_log(message):
+    print("-" * 18)
+    print(message)
+    print("-" * 18 + "\n")
 
 
 '''
@@ -15,9 +21,27 @@ File downloadded from web
 In this case it's a zip file
 '''
 class Webfile:
-    def __init__(self, url, file_name):
+    def __init__(self, url='', file_name=''):
         self.url = url
         self.file_name = file_name
+
+    def _already_downloaded(self):
+        try:
+            open(self.file_name)
+            return True
+        except FileNotFoundError as e:
+            super_log('Going to download file...')
+            return False
+
+    def _retreive(self):
+        try:
+            res = request.urlretrieve(self.url, self.file_name)
+        except Exception as e:
+            super_log("Was about to retreive %s, but got error: %s" \
+                      % (self.url, str(e)))
+            return None
+
+        return res
 
     '''
     Returns http.client.HTTPResponse
@@ -25,27 +49,30 @@ class Webfile:
     Or None if file had been already downloadded
     '''
     def download(self):
-        try:
-            open(FOLDER + "/" + COUNTRIES_FILE_NAME)
-        except:
-            return request.urlretrieve(self.url, self.file_name)
-        else:
+        if self._already_downloaded():
             return None
+        else:
+            return self._retreive()
 
 
 def download_countries():
-    # import ipdb; ipdb.set_trace()
-    Webfile(HOST + COUNTRIES + COUNTRIES_FILE_NAME,
-            FOLDER + '/' + COUNTRIES_FILE_NAME).download()
+    file_name = FOLDER + COUNTRIES_FILE_NAME
+
+    Webfile(url=HOST + COUNTRIES + COUNTRIES_FILE_NAME,
+            file_name=file_name).download()
+
+    if not os.path.exists(file_name):
+        super_log("Can't proceed, file %s not found" % file_name)
+        return 1
 
     # unzip zip file
-    with open(FOLDER + '/' + COUNTRIES_FILE_NAME, 'rb') as f:
+    with open(file_name, 'rb') as f:
         z = zipfile.ZipFile(f)
         for name in z.namelist():
             outpath = FOLDER + COUNTRIES_FILE_NAME_NO_EXT + "/"
             z.extract(name, outpath)
 
     # call org2org on unzipped stuff
-    os.system("ls -al")
+    os.system("ogr2ogr")
 
     # handle *.geojson file
