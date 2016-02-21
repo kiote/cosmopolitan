dfc4cc2e5b458e66d48e06226c17a75426ca96bf
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-02-21 13:06:08 +0300

    Some refactoring of import data for coutries
      to make code more readdable and reusable

--- cosmopolitan/management/commands/_naturalearthdata.py
+++ cosmopolitan/management/commands/_naturalearthdata.py
@@ -1,96 +1,43 @@
 import os
-import zipfile
-import json
-
-from urllib import request
 
 from cosmopolitan.models import Country
 from cosmopolitan.models import CountryGeoJSON
+
+import cosmopolitan.management.commands.service.web as sweb
+import cosmopolitan.management.commands.service.os as sos
+
 # countrise data URL
 # http://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_0_countries.zip
 
 HOST = "http://naciscdn.org"
-COUNTRIES = "/naturalearth/10m/cultural/"
-COUNTRIES_FILE_NAME_NO_EXT = "ne_10m_admin_0_countries"
-COUNTRIES_FILE_NAME = COUNTRIES_FILE_NAME_NO_EXT + ".zip"
 FOLDER = "data/"
 
-
-def _super_log(message):
-    print("-" * 18)
-    print(message)
-    print("-" * 18 + "\n")
-
-
-def _format_ogr2ogr(path='', file_name=''):
-    return "ogr2ogr -f 'GeoJSON' %(path)s.geojson %(path)s/%(file_name)s.shp" % \
-        {'path': path, 'file_name': file_name}
-
-
-class Webfile:
-    """
-    File downloaded from web
-
-    In this case it's a zip file
-    """
-    def __init__(self, url='', file_name=''):
-        self.url = url
-        self.file_name = file_name
-
-    def _already_downloaded(self):
-        try:
-            open(self.file_name)
-            return True
-        except FileNotFoundError as e:
-            _super_log('Going to download file...')
-            return False
-
-    def _retreive(self):
-        try:
-            res = request.urlretrieve(self.url, self.file_name)
-        except Exception as e:
-            _super_log("Was about to retreive %s, but got error: %s" \
-                       % (self.url, str(e)))
-            return None
-
-        return res
-
-    """
-    Returns http.client.HTTPResponse
-
-    Or None if file had been already downloadded
-    """
-    def download(self):
-        if self._already_downloaded():
-            return None
-        else:
-            return self._retreive()
-
+COUNTRIES = {
+                "http_path": "/naturalearth/10m/cultural/",
+                "file_name_without_extension": "ne_10m_admin_0_countries",
+                "file_name": "ne_10m_admin_0_countries.zip",
+            }
 
 def process_countries():
-    file_name = FOLDER + COUNTRIES_FILE_NAME
-    # import ipdb; ipdb.set_trace()
-    Webfile(url=HOST + COUNTRIES + COUNTRIES_FILE_NAME,
-            file_name=file_name).download()
+    file_name = FOLDER + COUNTRIES["file_name"]
+
+    sweb.Webfile(url=HOST + COUNTRIES["http_path"] + COUNTRIES["file_name"],
+                 file_name=file_name).download()
 
     if not os.path.exists(file_name):
-        _super_log("Can't proceed, file %s not found" % file_name)
+        sos._super_log("Can't proceed, file %s not found" % file_name)
         return 1
 
     # unzip zip file
-    with open(file_name, "rb") as f:
-        z = zipfile.ZipFile(f)
-        for name in z.namelist():
-            outpath = FOLDER + COUNTRIES_FILE_NAME_NO_EXT + "/"
-            z.extract(name, outpath)
+    outpath = FOLDER + COUNTRIES["file_name_without_extension"] + "/"
+    sos.Unzip(file_name=file_name, outpath=outpath).run()
 
     # call org2org on unzipped stuff
-    path = FOLDER + COUNTRIES_FILE_NAME_NO_EXT
-    os.system(_format_ogr2ogr(path=path, file_name=COUNTRIES_FILE_NAME_NO_EXT))
+    sos._format_ogr2ogr(path=FOLDER + COUNTRIES["file_name_without_extension"],
+                        file_name=COUNTRIES["file_name_without_extension"])
 
     # handle *.geojson file
-    with open(FOLDER + COUNTRIES_FILE_NAME_NO_EXT + ".geojson") as f:
-        data = json.load(f)
+    data = sos.LoadGeoJSON(FOLDER + COUNTRIES["file_name_without_extension"]).run()
 
     print("\n--- Seeding countries: ---")
 
